ad33a524db6afd2761e1977003d364991ce2dca9
Jethro_Block <jethroblock22@gmail.COM>
2024-02-07 12:09:54 +0000

    Add Icons component to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+import { Icons } from "./Icons";
 
 const Navbar = () => {
     return (
@@ -12,7 +13,7 @@ const Navbar = () => {
 
                             <div className="ml-4 flex lg:ml-0">
                                 <Link href="/">
-                                    
+                                    <Icons.logo className="h-10 w-10" />
                                 </Link>
                             </div>
                         </div>
