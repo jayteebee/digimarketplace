70fd6d945cb75e42a2365693923269da0991204a
Jethro_Block <jethroblock22@gmail.COM>
2024-02-07 16:01:03 +0000

    Add useRef hook to NavItems component

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import NavItem from "./NavItem";
 
 const NavItems = () => {
@@ -9,6 +9,9 @@ const NavItems = () => {
 
     const isAnyOpen = activeIndex !== null;
     
+
+    const navRef = useRef<HTMLDivElement | null>(null)
+
     return (
         <div className="flex gap-4 h-full">
             {PRODUCT_CATEGORIES.map((category, i) => {
