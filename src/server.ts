14ccc4b776cb92e2184cffb03f2040c4943901f6
Jethro_Block <jethroblock22@gmail.COM>
2024-02-07 17:21:35 +0000

    Add import statement and start function

--- src/server.ts
+++ src/server.ts
@@ -2,3 +2,9 @@ import express from 'express';
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
+
+const start = async () => {
+    const payload = await getPayloadClient()
+}
+
+start()
