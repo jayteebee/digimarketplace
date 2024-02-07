9b1513e3466e744edcde96f2bb85a6869bccf051
Jethro_Block <jethroblock22@gmail.COM>
2024-02-07 14:51:42 +0000

    Add featured items grid to NavItem component

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -31,6 +31,25 @@ const NavItem = ({isAnyOpen, category, isOpen, handleOpen}: NavItemProps) => {
                     "animate-in fade-in-10 slide-in-from-top-5": !isAnyOpen
                 })}>
                     <div className="absolute inset-0 top-1/2 bg-white shadow" aria-hidden="true" />
+
+                    <div className="relative">
+                        <div className="mx-auto max-w-7xl px-8">
+                            <div className="grid grid-cols-4 gap-x-8 gap-y-10 py-16">
+                                <div className="col-span-4 col-start-1 grid grid-cols-3 gap-x-8">
+                                    {category.featured.map((item) => (
+                                        <div
+                                        key={item.name}
+                                        className="group relative text-base sm:text-sm"
+                                        >
+                                            <div className="relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
+
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             ) : null}
         </div>
