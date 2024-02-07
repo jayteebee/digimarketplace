3fe3af4e21a6fab97ef6c8c27028b54af8a7a809
Jethro_Block <jethroblock22@gmail.COM>
2024-02-07 17:24:54 +0000

    Add global payload caching

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -5,4 +5,13 @@ dotenv.config({
     path: path.resolve(__dirname, "../.env")
 })
 
+let cached = (global as any).payload
+
+if(!cached) {
+    cached = (global as any).payload = {
+        client: null,
+        promise: null
+    }
+}
+
 export const getPayloadClient = async () => {}
