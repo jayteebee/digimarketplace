9c8a91d7bf0753724b9189ac4009fde8e5087f93
Jethro_Block <jethroblock22@gmail.COM>
2024-02-07 14:38:22 +0000

    Add ChevronDown component and cn utility function to NavItem

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { PRODUCT_CATEGORIES } from "@/config";
 import { Button } from "./ui/button";
+import { ChevronDown } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 type Category = typeof PRODUCT_CATEGORIES[number]
 
@@ -18,6 +20,9 @@ const NavItem = ({isAnyOpen, category, isOpen, handleOpen}: NavItemProps) => {
             <div className="relative flex items-center">
                 <Button className="gap-1.5" onClick={handleOpen} variant={isOpen ? "secondary" : "ghost"}>
                     {category.label}
+                    <ChevronDown className={cn("h-4 w-4 transition-all text-muted-foreground", {
+                        "-rotate-180" : isOpen,
+                    })} />
                 </Button>
             </div>
         </div>
