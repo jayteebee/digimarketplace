2306a05774e9619fb32e4d0648256882aedf2efd
Jethro_Block <jethroblock22@gmail.COM>
2024-02-07 17:34:10 +0000

    Add payload import and caching for getPayloadClient function

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import path from "path"
+import payload from "payload"
 import type { InitOptions } from "payload/config"
 
 dotenv.config({
@@ -23,4 +24,25 @@ export const getPayloadClient = async ({initOptions}: Args = {}) => {
     if(!process.env.PAYLOAD_SECRET) {
         throw new Error("PAYLOAD_SECRET is missing")
     }
+
+    if(cached.client) {
+        return cached.client
+    }
+
+    if(!cached.promise) {
+        cached.promise = payload.init({
+            secret: process.env.PAYLOAD_SECRET,
+            local: initOptions?.express ? false : true,
+            ...(initOptions || {})
+        })
+    }
+
+    try {
+        cached.client = await cached.promise
+    } catch (e: unknown) {
+        cached.promise = null
+        throw e
+    }
+
+    return cached.client
 }
