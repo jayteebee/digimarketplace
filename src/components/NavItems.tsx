929975beda622a8b9b57254e941dc1339a30f88d
Jethro_Block <jethroblock22@gmail.COM>
2024-02-07 12:22:39 +0000

    Delete unused SVG files and update NavItems component

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -6,7 +6,9 @@ const NavItems = () => {
     const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
     return (
-        <div className="flex gap-4 h-full"></div>
+        <div className="flex gap-4 h-full">
+            
+        </div>
     )
 }
 
