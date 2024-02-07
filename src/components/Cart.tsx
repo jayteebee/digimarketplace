de5f9d42fd37f065567c2a78146e798842f46684
Jethro_Block <jethroblock22@gmail.COM>
2024-02-07 16:32:36 +0000

    Update Cart component imports and add shopping cart icon

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,10 +1,11 @@
 "use client"
-import { Sheet } from "./ui/sheet";
+import { ShoppingCartIcon } from "lucide-react";
+import { Sheet, SheetTrigger } from "./ui/sheet";
 
 const Cart = () => {
     return (
         <Sheet>
-
+            <SheetTrigger><ShoppingCartIcon className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" /></SheetTrigger>
         </Sheet>
     )
 }
