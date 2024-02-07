bb2fdcf4185cbd2a58641fc6748cde316d66dcf2
Jethro_Block <jethroblock22@gmail.COM>
2024-02-07 16:04:45 +0000

    Add useOnClickOutside hook to handle click outside behavior

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -3,6 +3,7 @@
 import { PRODUCT_CATEGORIES } from "@/config";
 import { useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
     const [activeIndex, setActiveIndex] = useState<null | number>(null);
@@ -12,6 +13,8 @@ const NavItems = () => {
 
     const navRef = useRef<HTMLDivElement | null>(null)
 
+    useOnClickOutside(navRef, () => setActiveIndex(null))
+    
     return (
         <div className="flex gap-4 h-full">
             {PRODUCT_CATEGORIES.map((category, i) => {
