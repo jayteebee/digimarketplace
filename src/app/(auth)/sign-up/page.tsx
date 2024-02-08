336498855662bbb448abc52016455f09403cd4ba
Jethro_Block <jethroblock22@gmail.COM>
2024-02-08 14:42:41 +0000

    formatting

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -6,20 +6,23 @@ import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
-import {useForm} from 'react-hook-form'
-import {zodResolver} from '@hookform/resolvers/zod'
-import { AuthCredentialsValidator, TAuthCredentialsValidator } from "@/lib/validators/account-credentials-validator";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  AuthCredentialsValidator,
+  TAuthCredentialsValidator,
+} from "@/lib/validators/account-credentials-validator";
 
 const Page = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TAuthCredentialsValidator>({
+    resolver: zodResolver(AuthCredentialsValidator),
+  });
 
-const {register, handleSubmit, formState: {errors}} = useForm<TAuthCredentialsValidator>({
-  resolver: zodResolver(AuthCredentialsValidator),
-
-})
-
-const onSubmit = ({email, password }: TAuthCredentialsValidator) => {
-  
-}
+  const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {};
 
   return (
     <>
@@ -46,7 +49,7 @@ const onSubmit = ({email, password }: TAuthCredentialsValidator) => {
                 <div className="grid gap-1 py-2">
                   <Label htmlFor="email">Email</Label>
                   <Input
-                  {...register('email')}
+                    {...register("email")}
                     className={cn({
                       "focus-visible:ring-red-500": errors.email,
                     })}
@@ -57,14 +60,14 @@ const onSubmit = ({email, password }: TAuthCredentialsValidator) => {
                 <div className="grid gap-1 py-2">
                   <Label htmlFor="password">Password</Label>
                   <Input
-                  {...register('password')}
+                    {...register("password")}
                     className={cn({
                       "focus-visible:ring-red-500": errors.password,
                     })}
                     placeholder="PAssword"
                   />
                 </div>
-                    <Button>Sign up</Button>
+                <Button>Sign up</Button>
               </div>
             </form>
           </div>
