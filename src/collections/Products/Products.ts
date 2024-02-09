0936312cffaf42d402d623b518791dcb372f3bc6
Jethro_Block <jethroblock22@gmail.COM>
2024-02-09 17:23:11 +0000

    Add product fields and options for category and status

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -1,3 +1,4 @@
+import { PRODUCT_CATEGORIES } from "@/config";
 import { CollectionConfig } from "payload/types";
 
 export const Products: CollectionConfig = {
@@ -22,6 +23,60 @@ export const Products: CollectionConfig = {
             label: "Name",
             type: "text",
             required: true,
+        },
+        {
+            name: "description",
+            type: "textarea",
+            label: "Product Details",
+            required: true,
+        },
+        {
+            name: "price",
+            label: "Price in USD",
+            min: 0,
+            max: 1000,
+            type: "number",
+            required: true,
+        },
+        {
+            name: "category",
+            label: "Category",
+            type: "select",
+            options: PRODUCT_CATEGORIES.map(({label, value}) => ({label, value})),
+            required: true,
+        },
+        {
+            name: "product_files",
+            label: "Product File(s)",
+            type: "relationship",
+            relationTo: "product_files",
+            required: true,
+            hasMany: false,
+        },
+        {
+            name: "approvedForSale",
+            label: "Product Status",
+            type: "select",
+            defaultValue: "pending",
+            access: {
+                create: ({req}) => req.user.role === "admin",
+                read: ({req}) => req.user.role === "admin",
+                update: ({req}) => req.user.role === "admin"
+            },
+            options: [
+                {
+                    label: "Pending Verification",
+                    value: "pending",
+                },
+                {
+                    label: "Approved",
+                    value: "approved",
+                },
+                {
+                    label: "Rejected",
+                    value: "rejected",
+                }
+            ]
         }
     ]
     }
