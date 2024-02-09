642d5b4a74d8bbf0ccb881637e6bcc3b2ec7a4eb
Jethro_Block <jethroblock22@gmail.COM>
2024-02-09 16:48:11 +0000

    Update UserAccountNav component with dropdown menu and logout functionality

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import { DropdownMenuContent, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import { DropdownMenu } from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
 import { User } from "@/payload-types";
+import Link from "next/link";
 
 const UserAccountNav = ({user}: {user: User}) => {
 
@@ -15,9 +16,19 @@ const UserAccountNav = ({user}: {user: User}) => {
             <DropdownMenuContent className="bg-white w-60" align="end">
                 <div className="flex items-center justify-start gap-2 p-2">
                     <div className="flex flex-col space-y-0.5 leading-none">
-                        <p className="font-medium text-sm text-black">username</p>
+                        <p className="font-medium text-sm text-black">{user.email}</p>
                     </div>
                 </div>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem asChild>
+                    <Link href="/sell">Seller Dashboard</Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem className="cursor-pointer">
+                    Log out
+                </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
 )
