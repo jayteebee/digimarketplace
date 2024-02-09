7da5993bb149a60854bfcb0920a52da3a733dba8
Jethro_Block <jethroblock22@gmail.COM>
2024-02-09 18:48:29 +0000

    Add ProductFile collection configuration

---
+++ src/collections/ProductFile.ts
@@ -0,0 +1,34 @@
+import { User } from "../payload-types";
+import { BeforeChangeHook } from "payload/dist/collections/config/types";
+import { CollectionConfig } from "payload/types";
+
+const addUser: BeforeChangeHook = ({req,data}) => {
+    const user = req.user as User | null
+}
+
+export const ProductFiles: CollectionConfig = {
+    slug: "product_files",
+    admin: {
+        hidden: ({user}) => user.role !== "admin",
+    },
+    hooks: {
+        beforeChange: [addUser]
+    },
+    upload: {
+        staticURL: "/product_files",
+        staticDir: "product_files",
+        mimeTypes: ["image/*", "font/*", "application/postscript"],
+    },
+    fields: [
+        {
+            name: "user",
+            type: "relationship",
+            relationTo: "users",
+            required: true,
+            admin: {
+                condition: () => false,
+            },
+            hasMany: false,
+        }
+    ]
+}
