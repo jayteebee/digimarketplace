1c70d662ef8dfcccf1ee6c601e00b12dd52f5c85
Jethro_Block <jethroblock22@gmail.COM>
2024-02-09 17:23:23 +0000

    formatting

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -2,81 +2,81 @@ import { PRODUCT_CATEGORIES } from "@/config";
 import { CollectionConfig } from "payload/types";
 
 export const Products: CollectionConfig = {
-    slug: "products",
-    admin: {
-        useAsTitle: "name",
+  slug: "products",
+  admin: {
+    useAsTitle: "name",
+  },
+  access: {},
+  fields: [
+    {
+      name: "user",
+      type: "relationship",
+      relationTo: "users",
+      required: true,
+      hasMany: false,
+      admin: {
+        condition: () => false,
+      },
     },
-    access: {},
-    fields: [
+    {
+      name: "name",
+      label: "Name",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "description",
+      type: "textarea",
+      label: "Product Details",
+      required: true,
+    },
+    {
+      name: "price",
+      label: "Price in USD",
+      min: 0,
+      max: 1000,
+      type: "number",
+      required: true,
+    },
+    {
+      name: "category",
+      label: "Category",
+      type: "select",
+      options: PRODUCT_CATEGORIES.map(({ label, value }) => ({ label, value })),
+      required: true,
+    },
+    {
+      name: "product_files",
+      label: "Product File(s)",
+      type: "relationship",
+      relationTo: "product_files",
+      required: true,
+      hasMany: false,
+    },
+    {
+      name: "approvedForSale",
+      label: "Product Status",
+      type: "select",
+      defaultValue: "pending",
+      access: {
+        create: ({ req }) => req.user.role === "admin",
+        read: ({ req }) => req.user.role === "admin",
+        update: ({ req }) => req.user.role === "admin",
+      },
+      options: [
         {
-            name: "user",
-            type: "relationship",
-            relationTo: "users",
-            required: true,
-            hasMany: false,
-            admin: {
-                condition: () => false,
-            }
+          label: "Pending Verification",
+          value: "pending",
         },
         {
-            name: "name",
-            label: "Name",
-            type: "text",
-            required: true,
+          label: "Approved",
+          value: "approved",
         },
         {
-            name: "description",
-            type: "textarea",
-            label: "Product Details",
-            required: true,
+          label: "Rejected",
+          value: "rejected",
         },
-        {
-            name: "price",
-            label: "Price in USD",
-            min: 0,
-            max: 1000,
-            type: "number",
-            required: true,
-        },
-        {
-            name: "category",
-            label: "Category",
-            type: "select",
-            options: PRODUCT_CATEGORIES.map(({label, value}) => ({label, value})),
-            required: true,
-        },
-        {
-            name: "product_files",
-            label: "Product File(s)",
-            type: "relationship",
-            relationTo: "product_files",
-            required: true,
-            hasMany: false,
-        },
-        {
-            name: "approvedForSale",
-            label: "Product Status",
-            type: "select",
-            defaultValue: "pending",
-            access: {
-                create: ({req}) => req.user.role === "admin",
-                read: ({req}) => req.user.role === "admin",
-                update: ({req}) => req.user.role === "admin"
-            },
-            options: [
-                {
-                    label: "Pending Verification",
-                    value: "pending",
-                },
-                {
-                    label: "Approved",
-                    value: "approved",
-                },
-                {
-                    label: "Rejected",
-                    value: "rejected",
-                }
-            ]
-        }
-    ]
-    }
+      ],
+    },
+  ],
+};
