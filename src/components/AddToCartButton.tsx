89e8f3911a2e0df3067234971e6b323298577b1c
Jethro_Block <jethroblock22@gmail.COM>
2024-02-10 11:38:05 +0000

    Add import statement and state hook for success status

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,6 +1,11 @@
+"use client"
+import { useState } from "react"
 import { Button } from "./ui/button"
 
 const AddToCartButton = () => {
+const [isSuccess, setIsSuccess] = useState<boolean>(false)
+
+
     return (
         <Button size="lg" className="w-full" >
             Add to cart
