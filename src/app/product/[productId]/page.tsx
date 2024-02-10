acfc15a3fff303006eb229ffc66333fbc3d6ecc0
Jethro_Block <jethroblock22@gmail.COM>
2024-02-10 11:10:52 +0000

    Add breadcrumb arrow icon to navigation

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -34,6 +34,15 @@ const Page = ({params}: PageProps) => {
                                         <Link href={breadcrumb.href} className="font-medium text-sm text-muted-foreground hover:text-gray-900">
                                             {breadcrumb.name}
                                         </Link>
+                                        {i !== BREADCRUMBS.length - 1 ? (
+                                            <svg
+                                            viewBox='0 0 20 20'
+                                            fill='currentColor'
+                                            aria-hidden='true'
+                                            className='ml-2 h-5 w-5 flex-shrink-0 text-gray-300'>
+                                            <path d='M5.555 17.776l8-16 .894.448-8 16-.894-.448z' />
+                                        </svg>
+                                        ) : null}
                                     </div>
                                 </li>
                             ))}
