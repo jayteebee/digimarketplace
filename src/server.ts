b63895e0d387b470d5b28304660ebf5b3218ca55
Jethro_Block <jethroblock22@gmail.COM>
2024-02-10 16:28:26 +0000

    Add body-parser middleware and WebhookRequest type

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import { nextApp, nextHandler } from './next-utils';
 import * as trpcExpress from '@trpc/server/adapters/express'
 import { appRouter } from './trpc';
 import { inferAsyncReturnType } from '@trpc/server';
+import bodyParser from 'body-parser';
+import { IncomingMessage } from 'http';
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
@@ -14,7 +16,16 @@ const createContext = ({req,res}: trpcExpress.CreateExpressContextOptions) => ({
 
 export type ExpressContext = inferAsyncReturnType<typeof createContext>
 
+export type WebhookRequest = IncomingMessage & {rawBody: Buffer}
+
 const start = async () => {
+
+    const webhookMiddleware = bodyParser.json({
+        verify: (req: WebhookRequest, _, buffer) => {
+            req.rawBody = buffer
+        }
+    })
+
     const payload = await getPayloadClient({
         initOptions: {
             express: app,
