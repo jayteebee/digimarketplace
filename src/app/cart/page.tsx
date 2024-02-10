00adfb23540e9db767f18f5ef85b26641d815b64
Jethro_Block <jethroblock22@gmail.COM>
2024-02-10 13:13:40 +0000

    Add shopping cart page component

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,11 @@
+const Page = () => {
+    return (
+        <div className="bg-white">
+            <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 lg:max-w-7xl lg:px-8">
+                <h1 className="">Shopping Cart</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Page;
