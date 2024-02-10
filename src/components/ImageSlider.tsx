b0ecc01a63a771a969ade4da2df4f27a9138e149
Jethro_Block <jethroblock22@gmail.COM>
2024-02-10 10:36:58 +0000

    Add useEffect hook to track slide changes in ImageSlider component

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -4,7 +4,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import type SwiperType from "swiper";
 import { Pagination } from "swiper/modules";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface ImageSliderProps {
   urls: string[];
@@ -13,6 +13,20 @@ interface ImageSliderProps {
 const ImageSlider = ({ urls }: ImageSliderProps) => {
   const [swiper, setSwiper] = useState<null | SwiperType>(null);
   const [activeIndex, setActiveIndex] = useState(0);
+  const [slideConfig, setSlideConfig] = useState({
+    isBeginning: true,
+    isEnd: activeIndex === (urls.length ?? 0) - 1
+  })
+
+  useEffect(() => {
+    swiper?.on("slideChange", ({activeIndex}) => {
+        setActiveIndex(activeIndex)
+        setSlideConfig({
+            isBeginning: activeIndex === 0,
+            isEnd: activeIndex === (urls.length ?? 0) - 1
+        })
+    })
+  }, [swiper, urls])
 
   const activeStyle =
     "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
