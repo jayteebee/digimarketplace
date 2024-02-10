c4921e12728e6c7bcaabd97a3f33669a6eb54bf7
Jethro_Block <jethroblock22@gmail.COM>
2024-02-10 13:59:32 +0000

    Add order total to cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -156,6 +156,13 @@ const Page = () => {
                         {isMounted ? formatPrice(fee) : <Loader2 className="h-4 w-4 animate-spin text-muted-foreground" /> }
                     </div>
                 </div>
+
+                <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+                    <div className="text-base font-medium text-gray-900">Order Total</div>
+                    <div className="text-base font-medium text-gray-900">
+                    {isMounted ? formatPrice(cartTotal + fee) : <Loader2 className="h-4 w-4 animate-spin text-muted-foreground" /> }
+                    </div>
+                </div>
             </div>
           </section>
         </div>
