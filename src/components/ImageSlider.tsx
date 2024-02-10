301590c3d118cf3abce6867c2aafb0addd8f6076
Jethro_Block <jethroblock22@gmail.COM>
2024-02-10 10:32:07 +0000

    Add Swiper pagination and state hooks

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -2,12 +2,18 @@ import Image from 'next/image';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import "swiper/css"
 import "swiper/css/pagination"
+import type SwiperType from 'swiper'
+import {Pagination} from 'swiper/modules';
+import { useState } from 'react';
 
 interface ImageSliderProps {
     urls: string[];
 }
 
 const ImageSlider = ({urls}: ImageSliderProps) => {
+
+    const [swiper, setSwiper] = useState<null | SwiperType>(null)
+    const [activeIndex, setActiveIndex] = useState(0)
 
 const activeStyle = "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300"
 const inactiveStyles = "hidden text-gray-400"
@@ -19,7 +25,11 @@ const inactiveStyles = "hidden text-gray-400"
                 <button></button>
             </div>
 
-            <Swiper className='h-full w-full'>
+            <Swiper onSwiper={(swiper) => setSwiper(swiper)}
+            spaceBetween={50}
+            slidesPerView={1}
+            modules={[Pagination]}
+            className='h-full w-full'>
                 {urls.map((url, i) => 
                     <SwiperSlide key={i} className='-z-10 relative h-full w-full'>
                         <Image fill loading="eager" className='-z-10 h-full w-full object-cover object-center' src={url} alt="Product Image" />
