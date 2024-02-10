f427e100b1f43af854bd1f118f200f3fdf60551c
Jethro_Block <jethroblock22@gmail.COM>
2024-02-10 10:40:23 +0000

    Add image slider navigation buttons

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -5,6 +5,8 @@ import "swiper/css/pagination";
 import type SwiperType from "swiper";
 import { Pagination } from "swiper/modules";
 import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+import { ChevronRight } from "lucide-react";
 
 interface ImageSliderProps {
   urls: string[];
@@ -28,14 +30,19 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
     })
   }, [swiper, urls])
 
-  const activeStyle =
+  const activeStyles =
     "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
   const inactiveStyles = "hidden text-gray-400";
 
   return (
     <div className="group relative bg-zinc-100 aspect-square overflow-hidden rounded-xl">
       <div className="absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
-        <button></button>
+        <button className={cn(activeStyles, "right-3 transition", {
+            [inactiveStyles]: slideConfig.isEnd,
+            "hover:bg-primary-300 text-primary-800 opacity-100": !slideConfig.isEnd
+        })}
+        aria-label="next-image"
+        ><ChevronRight className="h-4 w-4 text-zinc-700" /></button>
         <button></button>
       </div>
 
