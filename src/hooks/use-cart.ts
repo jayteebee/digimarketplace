100a6e4920f31a303bcbd762a8d3d62bb80b4114
Jethro_Block <jethroblock22@gmail.COM>
2024-02-10 11:51:36 +0000

    Add persist middleware to useCart hook

--- src/hooks/use-cart.ts
+++ src/hooks/use-cart.ts
@@ -1,5 +1,6 @@
 import { Product } from "@/payload-types"
 import {create} from "zustand"
+import {persist} from "zustand/middleware"
 
 export type CartItem = {
     product: Product
@@ -13,4 +14,14 @@ type CartState = {
 
 }
 
-export const useCart = create<CartState>()
+export const useCart = create<CartState>()(
+    persist(
+        (set) => ({
+            items: [],
+            addItem: (product) => set((state) => ({items: [...state.items, {product}]})),
+        }),
+        removeItem: (id) => set((state) => ({items: state.items.filter((item) => item.product.id !== product.id)})),
+        clearCart: () => set({items: []}),
+        
+    )
+)
