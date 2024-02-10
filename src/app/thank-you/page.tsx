c32e1274c726f06adf3aec2cb477b5f8c96a2300
Jethro_Block <jethroblock22@gmail.COM>
2024-02-10 15:29:04 +0000

    Add server-side user authentication and retrieve order details

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,17 +1,40 @@
+import { getServerSideUser } from "@/lib/payload-utils"
 import Image from "next/image"
+import {cookies} from "next/headers"
+import { getPayloadClient } from "@/get-payload"
+import { notFound } from "next/navigation"
 
 interface PageProps {
     searchParams: {[key: string]: string | string[] | undefined}
 
 }
 
-const ThankYouPage = ({searchParams}: PageProps) => {
+const ThankYouPage = async ({searchParams}: PageProps) => {
 
     const orderId = searchParams.orderId
+    const nextCookies = cookies()
+    const {user} = await getServerSideUser(nextCookies)
+    const payload = await getPayloadClient()
+
+    const {docs: orders} = await payload.find({
+        collection: "orders",
+        depth: 2,
+        where: {
+            id: {
+                equals: orderId,
+            },
+        },
+    })
+
+    const [order] = orders
+
+    if(!order) return notFound()
+
+    const orderUserId = typeof order.user === "string" ? order.user : order.user.id
 
     return (
         <main className="relative lg:min-h-full">
-            <div className="hidden md:block h-80 overflow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12">
+            <div className="hidden lg:block h-80 overflow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12">
                 <Image fill src="/checkout-thank-you.jpg" className="h-full w-full object-cover object-center" alt="Thank you for your order" />
             </div>
 
