5388d8a1179c40946943e44d09b7c8b4c2a226b4
Jethro_Block <jethroblock22@gmail.COM>
2024-02-10 14:55:52 +0000

    Add payment functionality to cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -4,13 +4,26 @@ import { Button } from "@/components/ui/button";
 import { PRODUCT_CATEGORIES } from "@/config";
 import { useCart } from "@/hooks/use-cart";
 import { cn, formatPrice } from "@/lib/utils";
+import { trpc } from "@/trpc/client";
+
 import { Check, Loader2, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const Page = () => {
   const { items, removeItem } = useCart();
+
+  const router = useRouter()
+
+  const {mutate: createCheckoutsession, isLoading} = trpc.payment.createSession.useMutation({
+    onSuccess: ({url}) => {
+        if(url) router.push(url)
+    },
+  })
+
+  const productIds = items.map(({product}) => product.id )
 
   const [isMounted, setIsMounted] = useState<boolean>(false);
 
@@ -166,7 +179,9 @@ const Page = () => {
             </div>
 
             <div className="mt-6">
-                <Button className="w-full" size="lg">Checkout</Button>
+                <Button
+                onClick={() => createCheckoutsession({productIds})}
+                className="w-full" size="lg">Checkout</Button>
             </div>
           </section>
         </div>
