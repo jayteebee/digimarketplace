c6284227b5a5a0cdc52ec3474162dc90e3a9714e
Jethro_Block <jethroblock22@gmail.COM>
2024-02-10 11:39:21 +0000

    Add useEffect hook to handle success state timeout

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,10 +1,17 @@
 "use client"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Button } from "./ui/button"
 
 const AddToCartButton = () => {
 const [isSuccess, setIsSuccess] = useState<boolean>(false)
 
+useEffect(() => {
+    const timeout = setTimeout(() => {
+        setIsSuccess(false)
+    }, 2000)
+
+    return () => clearTimeout(timeout)
+}, [])
 
     return (
         <Button size="lg" className="w-full" >
