da8c2c2231c365cac262b23710b24000f2e2d046
Jethro_Block <jethroblock22@gmail.COM>
2024-02-10 09:53:52 +0000

    Fix formatting and add missing semicolons in ProductListing component

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Product } from "@/payload-types";
 import { useState } from "react";
@@ -7,35 +7,40 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 
 interface ProductListingProps {
-    product: Product | null
-    index: number
+  product: Product | null;
+  index: number;
 }
 
-const ProductListing = ({product, index}: ProductListingProps) => {
-    const [isVisible, setIsVisible] = useState<boolean>(false)
+const ProductListing = ({ product, index }: ProductListingProps) => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
 
-    if(!product || !isVisible) return <ProductPlaceholder />
+  
 
-    if(isVisible && product) {
-        return (
-            <Link className={cn("invisible h-full w-full cursor-pointer group/main", {
-                "visible animate-in fade-in-5": isVisible
-            })} href={`/product/${product.id}`}></Link>
-        )
-    }
-}
+  if (!product || !isVisible) return <ProductPlaceholder />;
+
+  if (isVisible && product) {
+    return (
+      <Link
+        className={cn("invisible h-full w-full cursor-pointer group/main", {
+          "visible animate-in fade-in-5": isVisible,
+        })}
+        href={`/product/${product.id}`}
+      ></Link>
+    );
+  }
+};
 
 const ProductPlaceholder = () => {
-    return (
-        <div className="flex flex-col w-full">
-            <div className="relative bg-zinc-100 aspect-square w-full overflow-hidden rounded-xl">
-                <Skeleton className="h-full w-full"/>
-            </div>
-            <Skeleton className="mt-4 w-2/3 h-4 rounded-lg" />
-            <Skeleton className="mt-2 w-16 h-4 rounded-lg" />
-            <Skeleton className="mt-2 w-12 h-4 rounded-lg" />
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col w-full">
+      <div className="relative bg-zinc-100 aspect-square w-full overflow-hidden rounded-xl">
+        <Skeleton className="h-full w-full" />
+      </div>
+      <Skeleton className="mt-4 w-2/3 h-4 rounded-lg" />
+      <Skeleton className="mt-2 w-16 h-4 rounded-lg" />
+      <Skeleton className="mt-2 w-12 h-4 rounded-lg" />
+    </div>
+  );
+};
 
 export default ProductListing;
