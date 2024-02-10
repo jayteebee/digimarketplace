85a2622ee2b4743af2f5681e4531a4f1d3b80aa8
Jethro_Block <jethroblock22@gmail.COM>
2024-02-10 08:37:35 +0000

    Refactor ProductReel component to include title section

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -1,3 +1,13 @@
-const ProductReel = () => {}
+const ProductReel = () => {
+    return (
+        <section className="py-12">
+            <div className="md:flex md:items-center md:justify-between mb-4">
+                <div className="max-w-2xl px-4 lg:max-w-4xl lg:px-0">
+                    Title
+                </div>
+            </div>
+        </section>
+    )
+}
 
 export default ProductReel
