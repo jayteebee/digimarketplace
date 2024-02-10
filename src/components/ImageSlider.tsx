777d0a0a67f03a0c8b64fe5c01ef40cd94b774b6
Jethro_Block <jethroblock22@gmail.COM>
2024-02-10 10:40:39 +0000

    formatting

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -17,18 +17,18 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [slideConfig, setSlideConfig] = useState({
     isBeginning: true,
-    isEnd: activeIndex === (urls.length ?? 0) - 1
-  })
+    isEnd: activeIndex === (urls.length ?? 0) - 1,
+  });
 
   useEffect(() => {
-    swiper?.on("slideChange", ({activeIndex}) => {
-        setActiveIndex(activeIndex)
-        setSlideConfig({
-            isBeginning: activeIndex === 0,
-            isEnd: activeIndex === (urls.length ?? 0) - 1
-        })
-    })
-  }, [swiper, urls])
+    swiper?.on("slideChange", ({ activeIndex }) => {
+      setActiveIndex(activeIndex);
+      setSlideConfig({
+        isBeginning: activeIndex === 0,
+        isEnd: activeIndex === (urls.length ?? 0) - 1,
+      });
+    });
+  }, [swiper, urls]);
 
   const activeStyles =
     "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
@@ -37,12 +37,16 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
   return (
     <div className="group relative bg-zinc-100 aspect-square overflow-hidden rounded-xl">
       <div className="absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
-        <button className={cn(activeStyles, "right-3 transition", {
+        <button
+          className={cn(activeStyles, "right-3 transition", {
             [inactiveStyles]: slideConfig.isEnd,
-            "hover:bg-primary-300 text-primary-800 opacity-100": !slideConfig.isEnd
-        })}
-        aria-label="next-image"
-        ><ChevronRight className="h-4 w-4 text-zinc-700" /></button>
+            "hover:bg-primary-300 text-primary-800 opacity-100":
+              !slideConfig.isEnd,
+          })}
+          aria-label="next-image"
+        >
+          <ChevronRight className="h-4 w-4 text-zinc-700" />
+        </button>
         <button></button>
       </div>
 
