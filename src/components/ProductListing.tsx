34986a8454414d632814c198ff2d7cda2ce6342c
Jethro_Block <jethroblock22@gmail.COM>
2024-02-10 09:53:18 +0000

    Add ProductListing component and Skeleton UI component

---
+++ src/components/ProductListing.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { Product } from "@/payload-types";
+import { useState } from "react";
+import { Skeleton } from "./ui/skeleton";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+
+interface ProductListingProps {
+    product: Product | null
+    index: number
+}
+
+const ProductListing = ({product, index}: ProductListingProps) => {
+    const [isVisible, setIsVisible] = useState<boolean>(false)
+
+    if(!product || !isVisible) return <ProductPlaceholder />
+
+    if(isVisible && product) {
+        return (
+            <Link className={cn("invisible h-full w-full cursor-pointer group/main", {
+                "visible animate-in fade-in-5": isVisible
+            })} href={`/product/${product.id}`}></Link>
+        )
+    }
+}
+
+const ProductPlaceholder = () => {
+    return (
+        <div className="flex flex-col w-full">
+            <div className="relative bg-zinc-100 aspect-square w-full overflow-hidden rounded-xl">
+                <Skeleton className="h-full w-full"/>
+            </div>
+            <Skeleton className="mt-4 w-2/3 h-4 rounded-lg" />
+            <Skeleton className="mt-2 w-16 h-4 rounded-lg" />
+            <Skeleton className="mt-2 w-12 h-4 rounded-lg" />
+        </div>
+    )
+}
+
+export default ProductListing;
