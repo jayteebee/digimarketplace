4ad15b3234f575e8dde0ebf1e78c3a5fae4d97a7
Jethro_Block <jethroblock22@gmail.COM>
2024-02-11 18:57:30 +0000

    Update Footer component with seller link and additional policies

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -31,12 +31,24 @@ const Footer = () => {
                                     <h3 className="font-semibold text-gray-900">Become a seller</h3>
                                     <p className="mt-2 text-sm text-muted-foreground">
                                         If you&apos;d like to sell high-quality digital products, you can do so in minutes.{` `}
-                                        <Link href="/sign-in?as=seller" className="whitespace-nowrap font-medium text-black hover:text-zinc-900">Get started</Link>
+                                        <Link href="/sign-in?as=seller" className="whitespace-nowrap font-medium text-black hover:text-zinc-900">Get started &rarr;</Link>
                                     </p>
                                 </div>
                             </div>
                         </div>
                     ) }
+                </div>
+                <div className="py-10 md:flex md:items-center md:justify-between">
+                    <div className="text-center md:text-left">
+                        <p className="text-sm text-muted-foreground">&copy; {new Date().getFullYear()} All Rights Reserved</p>
+                    </div>
+                    <div className="mt-4 flex items-center justify-center md:mt-0">
+                        <div className="flex space-x-8 ">
+                            <Link href="#" className="text-sm text-muted-foreground hover:text-gray-600">Cookie Policy</Link>
+                            <Link href="#" className="text-sm text-muted-foreground hover:text-gray-600">Terms Policy</Link>
+                            <Link href="#" className="text-sm text-muted-foreground hover:text-gray-600">Privacy Policy</Link>
+                        </div>
+                    </div>
                 </div>
             </MaxWidthWrapper>
         </footer>
