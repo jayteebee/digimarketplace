0a570e30b17a96a20b5d0f9f9e8aa2ef28988094
Jethro_Block <jethroblock22@gmail.COM>
2024-02-11 18:40:15 +0000

    Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { usePathname } from "next/navigation";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { Icons } from "./Icons";
+
+const Footer = () => {
+
+    const pathName = usePathname()
+    const pathsToMinimize = ["/verify-email", "/sign-up", "sign-in"]
+
+    return (
+        <footer className="bg-white flex-grow-0">
+            <MaxWidthWrapper>
+                <div className="border-t border-gray-200">
+                    {pathsToMinimize.includes(pathName) ? null : (
+                        <div className="flex justify-center">
+                            <Icons.logo className="h-12 w-auto" />
+                        </div>
+                    )}
+                </div>
+            </MaxWidthWrapper>
+        </footer>
+    )
+}
+
+export default Footer;
